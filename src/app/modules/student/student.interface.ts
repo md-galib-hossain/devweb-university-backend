77116e85bd45ea128d9074a82e9bc46ad548d309
md-galib-hossain <mdgalib23@gmail.com>
2024-02-18 12:50:08 +0600

    Student interface & model create

---
+++ src/app/modules/student/student.interface.ts
@@ -0,0 +1,41 @@
+export type TGuardian = {
+    
+    fatherName : string;
+    fatherOccupation : string;
+    fatherContactNo : string;
+    motherName : string;
+    motherOccupation : string;
+    motherContactNo? : string
+
+}
+
+export type TUserName = {
+firstName : string;
+middleName : string;
+lastName : string;
+}
+
+export type TLocalGuardian = {
+name : string;
+occupation : string;
+contactNo : string;
+address : string
+}
+
+export type TStudent = {
+name : TUserName;
+email : string;
+id : string;
+gender : "male" | "female";
+dateOfBirth? : string;
+contactNo : string;
+emergencyContactNo: string;
+bloodGroup? : "A+" | "A-" | "B+" | "B-" | "O+" | "O-" | "AB+" | "AB-" ;
+presentAddress : string;
+permanentAddress : string;
+guardian:TGuardian;
+localGuardian : TLocalGuardian;
+profileImg? : string;
+isActive : "active" | "blocked"
+
+}
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,12 @@
 
 import mongoose from 'mongoose'
-import config from "./config"
+import config from "./app/config"
 import app from './app'
 
 
     async function main() {
         try{
-        await mongoose.connect(config.DATABASE_URL);
+        await mongoose.connect(config.DATABASE_URL as string);
         console.log("connected db")
         app.listen(config.port, () => {
             console.log(`Example app listening at http://localhost:${config.port}`)
