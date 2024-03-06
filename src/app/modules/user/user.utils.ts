e3a09d5258d0e5bfa1f5c614533b76f46fd45c5e
md-galib-hossain <mdgalib23@gmail.com>
2024-03-06 11:46:27 +0600

    student id generator incremenetal bug fix

--- src/app/modules/user/user.utils.ts
+++ src/app/modules/user/user.utils.ts
@@ -10,14 +10,27 @@ const findLastStudentId = async () => {
       id: 1,
       _id: 0,
     }
-  ).sort({createdAt : -1})
-  .lean();
-  return lastStudent?.id ? lastStudent.id.substring(6) : undefined
+  )
+    .sort({ createdAt: -1 })
+    .lean();
+  return lastStudent?.id ? lastStudent.id : undefined;
 };
 
 //year semesterCode 4 digit number
-export const generateStudentId =async (payload: TAcademicSemester) => {
-  const currentId = await findLastStudentId() || (0).toString();
+export const generateStudentId = async (payload: TAcademicSemester) => {
+  let currentId = (0).toString(); //default
+  const lastStudentId = await findLastStudentId();
+  const lastStudentSemesterCode = lastStudentId?.substring(4, 6);
+  const lastStudentYear = lastStudentId?.substring(0, 4);
+  const currentSemesterCode = payload.code;
+  const currentYear = payload.year;
+  if (
+    lastStudentId &&
+    lastStudentSemesterCode === currentSemesterCode &&
+    lastStudentYear === currentYear
+  ) {
+    currentId = lastStudentId.substring(6)
+  }
   //2030010001
   let incremenentId = (Number(currentId) + 1).toString().padStart(4, "0");
   incremenentId = `${payload.year}${payload.code}${incremenentId}`;
