bd8c26cfa50644b71384ef3e2454906b7811ab4d
md-galib-hossain <mdgalib23@gmail.com>
2024-03-07 11:29:55 +0600

    Create Custom App Error

--- src/app/modules/academicDepartment/academicDepartment.model.ts
+++ src/app/modules/academicDepartment/academicDepartment.model.ts
@@ -19,26 +19,37 @@ const academicDepartmentSchema = new Schema<TAcademicDepartment>(
   }
 );
 
-academicDepartmentSchema.pre('save', async function(next){
-const isDepartmentExist = await AcademicDepartment.findOne({
-    name : this.name
-})
-if(isDepartmentExist){
-    throw new Error('Department already exists')
+class AppError extends Error {
+  public statusCode: number;
+  constructor(statusCode: number, message: string, stack?: "") {
+    super(message);
+    this.statusCode = statusCode;
+    if (stack) {
+      this.stack = stack;
+    } else {
+      Error.captureStackTrace(this, this.constructor);
+    }
+  }
 }
-next()
 
-})
+academicDepartmentSchema.pre("save", async function (next) {
+  const isDepartmentExist = await AcademicDepartment.findOne({
+    name: this.name,
+  });
+  if (isDepartmentExist) {
+    throw new AppError(404,'This department already exists')
+  }
+  next();
+});
 
-academicDepartmentSchema.pre('findOneAndUpdate', async function(next){
-  const query = this.getQuery()
-  const isDepartmentExist = await AcademicDepartment.findOne(query)
-if(!isDepartmentExist){
-    throw new Error('Department does not exist')
-}
-next()
-    })
-
+academicDepartmentSchema.pre("findOneAndUpdate", async function (next) {
+  const query = this.getQuery();
+  const isDepartmentExist = await AcademicDepartment.findOne(query);
+  if (!isDepartmentExist) {
+    throw new AppError(404,'This department does not exist')
+  }
+  next();
+});
 
 export const AcademicDepartment = model<TAcademicDepartment>(
   "AcademicDepartment",
